ea68775ca4862025e74db623f659139cf5325228
細川健 <wall.raccoon@gmail.com>
2024-12-24 03:38:43 +0900

    frontend_backup削除

--- frontend-backup/src/app/(protected)/@rightSideBar/rightSideBar.tsx
+++
@@ -1,9 +0,0 @@
-import UserSearch from "./userSearch";
-
-export default function RightSidebar() {
-  return (
-    <div className="w-80 p-4 space-y-8 bg-white rounded-lg shadow-lg">
-      <UserSearch />
-    </div>
-  );
-}
